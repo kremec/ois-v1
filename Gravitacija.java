0a6649f0cfd670693ab2fd76a5701088503d97e8
kapa28 <kapa28slav@gmail.com>
2023-02-21 16:53:26 +0100

    Dodaj izpis #4

--- Gravitacija.java
+++ Gravitacija.java
@@ -1,7 +1,10 @@
 public class Gravitacija {
     
     public static void main(String[] args){
-
 		System.out.println("OIS je zakon!");
+
+		static void nadmorskaVisina(double visina, double grav_pospesek) {
+            System.out.println(visina + " " + grav_pospesek);
+        }
 	}
 }
