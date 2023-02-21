d5ae11128e74089a17d096f8a365299e6eacc8be
AlesPozar <ales.pozar003@gmail.com>
2023-02-21 16:53:19 +0100

    Dodaj izračun pospeška #3

--- Gravitacija.java
+++ Gravitacija.java
@@ -1,7 +1,13 @@
+import java.lang.Math.*;
 public class Gravitacija {
     
     public static void main(String[] args){
 
 		System.out.println("OIS je zakon!");
+	}
+
+	public static double gravPos(double visina)
+	{
+		return (6.674*Math.pow(10,-11)*5.972*Math.pow(10,24))/(Math.pow(6.371*Math.pow(10,6) + visina,2));
 	}
 }
