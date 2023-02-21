35b135f7e3bf623e381e026aaba05f0373eb51f5
kapa28 <108547693+kapa28@users.noreply.github.com>
2023-02-21 17:14:27 +0100

    Update Gravitacija.java
    Popravek merga.

--- Gravitacija.java
+++ Gravitacija.java
@@ -3,14 +3,14 @@ public class Gravitacija {
     
     public static void main(String[] args){
 		System.out.println("OIS je zakon!");
-
-		static void nadmorskaVisina(double visina, double grav_pospesek) {
-            System.out.println(visina + " " + grav_pospesek);
-        }
 	}
 
 	public static double gravPos(double visina)
 	{
 		return (6.674*Math.pow(10,-11)*5.972*Math.pow(10,24))/(Math.pow(6.371*Math.pow(10,6) + visina,2));
 	}
+	
+	public static void nadmorskaVisina(double visina, double grav_pospesek) {
+            System.out.println(visina + " " + grav_pospesek);
+        }
 }
